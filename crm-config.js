aa1350325b6570b89bd0abdf4cd871fe489880a5
Vladyslav <shv.vladyslav@gmail.com>
2025-12-25 16:12:11 +0100

    Create crm-config.js

---
+++ crm-config.js
@@ -0,0 +1,12 @@
+// crm-config.js
+// PR-14 CRM Live
+window.UHU_CRM = {
+  // Вставь сюда URL своего Google Apps Script Web App (/exec)
+  endpoint: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+
+  // включатели (можно оставить true)
+  enabled: true,
+
+  // куда отправлять (минимум)
+  mode: "no-cors", // важно для GitHub Pages/статического сайта
+};
