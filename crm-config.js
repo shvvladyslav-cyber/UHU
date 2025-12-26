2c775b4ac055331e9599c4d105b26c42a024bb7e
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 23:24:58 +0100

    Delete crm-config.js

--- crm-config.js
+++
@@ -1,9 +0,0 @@
-// crm-config.js
-window.UHU_CRM = {
-  // сюда вставь Web app URL из Apps Script (Deploy → Web app URL)
-  endpoint: "https://script.google.com/macros/s/AKfycbwGZ7PT8ZNDWBCF1n-k81N0_nmPQme2CZxKRWa_Yp0wDxsb6pjM4KulNnl70AulxjWo/exec",
-  enabled: true,
-
-  // важно: для Apps Script обычно лучше no-cors (чтобы сайт не падал из-за CORS)
-  mode: "no-cors",
-};
