2d727e8864323c2f3d2d7922b85a75796669c016
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 21:10:05 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,7 +1,9 @@
 // crm-config.js
-// PR-14 CRM Live
 window.UHU_CRM = {
+  // сюда вставь Web app URL из Apps Script (Deploy → Web app URL)
   endpoint: "https://script.google.com/macros/s/AKfycbxeN8spv04Am9LxgtaPTNx7PLW3eqxeY3mnHqWWERk6xC26SX3eVGzhPXdfP8lrqCEC3g/exec",
   enabled: true,
+
+  // важно: для Apps Script обычно лучше no-cors (чтобы сайт не падал из-за CORS)
   mode: "no-cors",
 };
