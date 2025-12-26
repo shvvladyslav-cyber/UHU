8763f29b9dd1c90c69918896a4fecdda27fa49c4
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 21:36:29 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,7 +1,7 @@
 // crm-config.js
 window.UHU_CRM = {
   // сюда вставь Web app URL из Apps Script (Deploy → Web app URL)
-  endpoint: "https://script.google.com/macros/s/AKfycbxeN8spv04Am9LxgtaPTNx7PLW3eqxeY3mnHqWWERk6xC26SX3eVGzhPXdfP8lrqCEC3g/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbwGZ7PT8ZNDWBCF1n-k81N0_nmPQme2CZxKRWa_Yp0wDxsb6pjM4KulNnl70AulxjWo/exec",
   enabled: true,
 
   // важно: для Apps Script обычно лучше no-cors (чтобы сайт не падал из-за CORS)
