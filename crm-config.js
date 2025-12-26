5d0030b744aa15f34114767cecf07d78e7c47d7f
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 19:29:53 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,8 +1,5 @@
-// crm-config.js
 window.UHU_CRM = {
   enabled: true,
   endpoint: "https://script.google.com/macros/s/AKfycbxAQ7XZBLMveUhGWWQATCAxxiC-3fEJNxNBppEmyPJkypegFxn0EaZ_iRQlmCaA0Pw/exec",
-  // лучше так: отправляем, но не читаем ответ (чтобы не ловить CORS)
   mode: "no-cors",
 };
-
