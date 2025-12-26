0c8ae0cc923a596aea9b1e1c03e00498d956015b
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 20:50:32 +0100

    Delete crm-config.js

--- crm-config.js
+++
@@ -1,5 +0,0 @@
-window.UHU_CRM = {
-  enabled: true,
-  endpoint: "https://script.google.com/macros/s/AKfycbxAQ7XZBLMveUhGWWQATCAxxiC-3fEJNxNBppEmyPJkypegFxn0EaZ_iRQlmCaA0Pw/exec",
-  mode: "no-cors",
-};
