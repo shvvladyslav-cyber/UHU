710f42db0f7d6ba9dff081ef60215afeb67c8c45
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 15:10:48 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,7 +1,7 @@
 // crm-config.js
 // PR-14 CRM Live
 window.UHU_CRM = {
-  endpoint: "https://script.google.com/macros/s/AKfycbxeN8spv04Am9LxgtaPTNx7PLW3eqxeY3mnHqWWERk6xC26SX3eVGzhPXdfP8lrqCEC3g/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbzu8j8qqK7DA9i2FKbRoQ1QPDqDjIb1iDwfIsdTsRKHi15bvO6Sdp0CaP8Bj_vLb6lm/exec",
   enabled: true,
   mode: "no-cors",
 };
