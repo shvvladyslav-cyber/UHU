8c041e47b871b58c1661ea1dff499383335d5925
Vladyslav <shv.vladyslav@gmail.com>
2025-12-26 18:27:43 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,5 +1,4 @@
 window.UHU_CRM = {
-  endpoint: "https://script.google.com/macros/s/AKfycbxAQ7XZBLMveUhGWWQATCAxxiC-3fEJNxNBppEmyPJkypegFxn0EaZ_iRQlmCaA0Pw/exec",
-  enabled: true,
-  mode: "no-cors",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbxAQ7XZBLMveUhGWWQATCAxxiC-3fEJNxNBppEmyPJkypegFxn0EaZ_iRQlmCaA0Pw/exec",
+  source: "uhu.digital"
 };
