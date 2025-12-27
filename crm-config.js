f5cb93a1fc70874e02f872285425b80f240cc2b7
Vladyslav <shv.vladyslav@gmail.com>
2025-12-27 16:15:03 +0100

    Delete crm-config.js

--- crm-config.js
+++
@@ -1,7 +0,0 @@
-// crm-config.js
-// 1) Вставь сюда URL твоего Google Apps Script Web App (после Deploy → Web app)
-window.UHU_CRM = {
-  endpoint: "https://script.google.com/macros/s/AKfycbzYCiG40gwbQrFWX9q02Zsh8uNWbFWlS_r5ifmF0tiwR9r_sv606USS79Y0rweYeN78/exec", // например: https://script.google.com/macros/s/XXXXX/exec
-  site: "UHU.digital",
-  currency: "EUR"
-};
