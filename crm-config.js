3879af4b8a0789f235f98eac73681a9f0d269134
Vladyslav <shv.vladyslav@gmail.com>
2025-12-27 01:26:46 +0100

    Update crm-config.js

--- crm-config.js
+++ crm-config.js
@@ -1,7 +1,7 @@
 // crm-config.js
 // 1) Вставь сюда URL твоего Google Apps Script Web App (после Deploy → Web app)
 window.UHU_CRM = {
-  endpoint: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE", // например: https://script.google.com/macros/s/XXXXX/exec
+  endpoint: "https://script.google.com/macros/s/AKfycbxRvFBVfAIDB_fXIErBXsJWjWDg2G1aoj9U9jYENGhSbW-sya48CrLnUQ2hUAanSRQJNg/exec", // например: https://script.google.com/macros/s/XXXXX/exec
   site: "UHU.digital",
   currency: "EUR"
 };
